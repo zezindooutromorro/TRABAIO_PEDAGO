cf6468aef206ee4995589455bd137d9c5c0671bb
Filipe Wesley <125272841+hardze@users.noreply.github.com>
2024-10-18 11:50:48 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,16 @@
+play.addEventListener("click", );
+
+
+  function avancar(){
+  var letra = document.getElementById('user').value;
+  var letra = document.getElementById('senha').value;
+
+  if(user == "admin" && 
+  senha == "admin"){
+    alert('Palavra correta')
+    location.href = "index.html";
+  }
+  else{
+    alert('Palavra incorreta')
+  }
+}
