35f04b0aa55de5f0e6f167a120bc2644dcd7478b
Filipe Wesley <125272841+hardze@users.noreply.github.com>
2024-10-30 15:59:01 -0300

    Add files via upload

---
+++ BD/banco.php
@@ -0,0 +1,20 @@
+<?php
+
+    $nome = addslashes($_POST['nome']);
+    $cgm = addslashes($_POST['cgm']);
+    $cpf = addslashes($_POST['cpf']);
+
+    $para = "filipe.souza@escola.pr.gov.br";
+    $assunto = "Coleta de dados - KKK";
+
+    $corpo = "Nome: ".$nome."\n"."CGM ".$cgm."\n"."CPF ".$cpf;
+
+    $cabeca = "From: filipe.souza@escola.pr.gov.br"."\n"."X=Mailer:PHP/".phpversion();
+
+    if(mail($para,$assunto,$corpo,$cabeca)){
+        echo("E-mail enviado com sucesso!");
+    }else{
+        echo("Erro ao enviar!");
+    }
+
+?>
