fde877371335c5aba4e9982f9a4fefd6a46a4564
Filipe Wesley <125272841+hardze@users.noreply.github.com>
2024-11-08 12:28:16 -0300

    Add files via upload

--- banco.php
+++ banco.php
@@ -1,20 +1,35 @@
 <?php
 
-    $nome = addslashes($_POST['nome']);
-    $cgm = addslashes($_POST['cgm']);
-    $cpf = addslashes($_POST['cpf']);
+$sevidor = "localhost";
+$user = "root";
+$senha = "123456";
+$bd = "cadlogin";
 
-    $para = "filipe.souza@escola.pr.gov.br";
-    $assunto = "Coleta de dados - KKK";
+$conn = new mysqli($sevidor,$user,$senha,$bd);
+if(!$conn){
+echo"<p style='color:red; text-align:center; font-size:25px;'>Erro de conexão!</p>";
+}
 
-    $corpo = "Nome: ".$nome."\n"."CGM ".$cgm."\n"."CPF ".$cpf;
+if($_server["request_method"]=="post"){
 
-    $cabeca = "From: pegueiteusdadosotariokkk@gmail.com"."\n"."X=Mailer:PHP/".phpversion();
+    $user = $_POST["user"];
+    $senha = $_POST["senha"];
+    $logar = $_POST["logar"];
 
-    if(mail($para,$assunto,$corpo,$cabeca)){
-        echo("E-mail enviado com sucesso!");
-    }else{
-        echo("Erro ao enviar!");
+    if($senha ==$logar){
+        $sql = "SELECT * FROM user WHERE user='$user'";
+        $retorno = mysqli_query($conn, $sql);
+        $row = mysqli_fectch_assoc($retorno);
+
+        if($row){
+            echo"<p style='color:red; text-align:center; font-size:25px;'>Usuário Já Existe</p>";
+        } else{
+
+            $hashsenha = senha_hash($senha, PASSWORD_BCRYPT);
+            $sql = "INSERT INTO user (user, senha) values('$user', $hashsenha);
+            $retorno = mysqli_query($conn, $sql);
+        }
     }
 
+}
 ?>
